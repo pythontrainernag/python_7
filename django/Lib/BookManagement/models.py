eaff90ac6885f3c1e1db91d885215040b32d8f0f
NageswaraRao <pythontrainernag@gmail.com>
2019-02-19 07:56:18 +0530

    19/02/2019

--- django/Lib/BookManagement/models.py
+++ django/Lib/BookManagement/models.py
@@ -2,21 +2,30 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.template.defaultfilters import default
 
 # Create your models here.
 
-class Book(models.Model):
-    name = models.CharField(max_length=50)
+
 
 class Author(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField()
-    book = models.ManyToManyField(Book)
+    
+    def __str__(self):
+        return self.first_name+self.last_name
 
 class Publisher(models.Model):
     name = models.CharField(max_length=50)
     address = models.TextField()
     website = models.CharField(max_length=50)
-    books = models.ForeignKey(Book)
-        
+    def __str__(self):
+        return self.name
+    
+class Book(models.Model):
+    name = models.CharField(max_length=50)
+    publisher = models.ForeignKey(Publisher, default=0)
+    authors = models.ManyToManyField(Author)
+    def __str__(self):
+        return self.name
